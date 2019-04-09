390af94f7894b9bceb2dcd792c71dc273d9518c8
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2019-04-09 15:29:13 +0200

    Update README

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,11 +1,32 @@
 const { Command } = require('@oclif/command');
+const { execSync } = require('child_process');
+const fs = require('fs-extra');
+const commandExistsSync = require('command-exists').sync;
 
 class InitCommand extends Command {
   async run() {
     const { args } = this.parse(InitCommand);
-    // TODO: implement react-native init and directories/files generation
     this.log(`Run \`react-native init ${args.name}\``);
     this.log('Generate white-label directories and files');
+    if (!commandExistsSync('react-native')) {
+      this.log('react-native command, not found. This CLI requires react-native to be installed');
+      this.exit(1);
+    }
+    if (fs.existsSync(args.name)) {
+      this.log(`Directory ${args.name} already exists.`);
+      this.exit(1);
+    }
+
+    // to test, we can use this: execSync(`mkdir ${args.name}`);
+    // execSync(`react-native init ${args.name}`);
+    execSync(`mkdir ${args.name}`);
+
+    const wlDirectories = ['modules', 'namedModules', 'themes', 'contexts', '.rayctl'];
+    wlDirectories.forEach(directoryName => {
+      fs.mkdirSync(`${args.name}/${directoryName}`);
+    });
+
+    fs.writeFileSync(`${args.name}/.rayctl/config.json`, '');
   }
 }
 
--- test/commands/init.test.js
+++ test/commands/init.test.js
@@ -1,6 +1,19 @@
+const fs = require('fs-extra');
 const { expect, test } = require('@oclif/test');
 
+const cleanInitFolder = () => {
+  if (fs.existsSync('foo')) {
+    fs.removeSync('foo');
+  }
+};
+
 describe('init', () => {
+  /* eslint-disable no-undef */
+  afterEach(() => {
+    cleanInitFolder();
+  });
+  /* eslint-enable no-undef */
+
   test
     .command(['init'])
     .exit(2)
@@ -9,7 +22,40 @@ describe('init', () => {
   test
     .stdout()
     .command(['init', 'foo'])
-    .it('crashes without argument', ctx => {
+    .it('message log appears', ctx => {
       expect(ctx.stdout).to.contain('Run `react-native init foo`');
     });
+
+  test
+    .do(() => {
+      fs.mkdirSync('foo');
+    })
+    .stdout()
+    .command(['init', 'foo'])
+    .exit(1)
+    .it('init folder already exists', ctx => {
+      expect(ctx.stdout).to.contain('Directory foo already exists.');
+    });
+
+  test.command(['init', 'foo']).it('creates foo folder', () => {
+    const fooFolderExists = fs.existsSync('foo');
+    expect(fooFolderExists).to.equal(true);
+  });
+
+  test.command(['init', 'foo']).it('creates correct wl structure', () => {
+    const modulesDirectory = fs.existsSync('foo/modules');
+    const namedModulesDirectory = fs.existsSync('foo/namedModules');
+    const themesDirectory = fs.existsSync('foo/themes');
+    const contextsDirectory = fs.existsSync('foo/contexts');
+    const rayctlDirectory = fs.existsSync('foo/.rayctl');
+
+    expect(modulesDirectory).to.equal(true);
+    expect(namedModulesDirectory).to.equal(true);
+    expect(themesDirectory).to.equal(true);
+    expect(contextsDirectory).to.equal(true);
+    expect(rayctlDirectory).to.equal(true);
+
+    const rayctlConfig = fs.existsSync('foo/.rayctl/config.json');
+    expect(rayctlConfig).to.equal(true);
+  });
 });
